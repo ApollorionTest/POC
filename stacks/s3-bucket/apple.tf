8ea8c1b9b2b2bed8fdef09f97a42aa20e181796b
ApollorionTest <ApollorionTest>
2025-08-25 11:44:35 -0400

    feat: create S3 bucket apple

---
+++ stacks/s3-bucket/apple.tf
@@ -0,0 +1,11 @@
+module apple {
+  source = "github.com/thoughtbot/terraform-s3-bucket?ref=v0.4.0"
+
+  name = "apple"
+  tags = { 
+    
+    "apple" = "banana",
+    
+    "test" = "test",
+      }
+}
