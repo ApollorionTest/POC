8d8d60b1b77f26251b1573228aff74e44a104a75
ApollorionTest <ApollorionTest>
2025-08-25 14:05:36 -0400

    feat: create S3 bucket scary-terry

---
+++ stacks/s3-bucket/scary-terry.tf
@@ -0,0 +1,11 @@
+module scary-terry {
+  source = "github.com/thoughtbot/terraform-s3-bucket?ref=v0.4.0"
+
+  name = "scary-terry"
+  tags = { 
+    
+    "demo" = "poc",
+    
+    "terry" = "isscary",
+      }
+}
