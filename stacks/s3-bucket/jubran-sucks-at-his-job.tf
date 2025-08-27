61f8916044341d059150e234a14cf00626033739
ApollorionTest <ApollorionTest>
2025-08-27 10:57:10 -0400

    feat: create S3 bucket jubran-sucks-at-his-job

---
+++ stacks/s3-bucket/jubran-sucks-at-his-job.tf
@@ -0,0 +1,11 @@
+module jubran-sucks-at-his-job {
+  source = "github.com/thoughtbot/terraform-s3-bucket?ref=v0.4.0"
+
+  name = "jubran-sucks-at-his-job"
+  tags = { 
+    
+    "joey" = "isgreat",
+    
+    "jubran" = "isbad",
+      }
+}
